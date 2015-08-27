aa64aee3ae5bf8482e3effc0230e09bcfa85de25
Max Koreychenko <koreychenko@gmail.com>
2015-08-27 10:10:11 +0300

    initial commit

---
+++ callbackform-template.tpl.php
@@ -0,0 +1,7 @@
+<div class="form-group">
+  <?php print render($form['phone']); ?>
+</div>
+<div class="form-group">
+  <?php print render($form['submit']); ?>
+</div>
+<?php print drupal_render_children($form); ?>
